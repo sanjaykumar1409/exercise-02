d1ae76a28c688afd2ab0d4aef5378fc671ff4802
sanjay kumar <141133625+sanjaykumar1409@users.noreply.github.com>
2023-09-01 21:33:29 +0530

    Create exercise-02.py

---
+++ exercise-02.py
@@ -0,0 +1,8 @@
+age = input("What is your current age?\n")
+a=int(age)
+y=90-a
+months=y*12
+days=y*365
+weeks=y*52
+print(f"if you live till 90 years you have {months} months with {weeks} weeks and {days} days")
+
